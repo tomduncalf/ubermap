223f2c59193721a86309d26098fcc2421fa5ad04
Benoit Besson <benoi@P3520>
2018-02-11 10:08:25 +0100

    fix push init files patches for live 10

--- Devices/Push/__init__.py
+++ Devices/Push/__init__.py
@@ -1,13 +1,15 @@
-# Embedded file name: /Users/versonator/Jenkins/live/output/mac_64_static/Release/python-bundle/MIDI Remote Scripts/Push/__init__.py
-from __future__ import absolute_import, print_function
+# Embedded file name: c:\Jenkins\live\output\win_64_static\Release\python-bundle\MIDI Remote Scripts\Push\__init__.py
+from __future__ import absolute_import, print_function, unicode_literals
 from ableton.v2.control_surface.capabilities import controller_id, inport, outport, AUTO_LOAD_KEY, CONTROLLER_ID_KEY, FIRMWARE_KEY, HIDDEN, NOTES_CC, PORTS_KEY, SCRIPT, SYNC, TYPE_KEY
 from .firmware_handling import get_provided_firmware_version
 from .push import Push
 
-from Ubermap import UbermapDevicesPatches
+#FIX TO WORK WITH CLYPHX
+#from Ubermap import UbermapDevicesPatches
+#END OF FIX
 
 def get_capabilities():
-    return {CONTROLLER_ID_KEY: controller_id(vendor_id=2536, product_ids=[21], model_name='Ableton Push'),
+    return {CONTROLLER_ID_KEY: controller_id(vendor_id=2536, product_ids=[21], model_name=u'Ableton Push'),
      PORTS_KEY: [inport(props=[HIDDEN, NOTES_CC, SCRIPT]),
                  inport(props=[]),
                  outport(props=[HIDDEN,
@@ -15,13 +17,19 @@ def get_capabilities():
                   SYNC,
                   SCRIPT]),
                  outport(props=[])],
-     TYPE_KEY: 'push',
+     TYPE_KEY: u'push',
      FIRMWARE_KEY: get_provided_firmware_version(),
      AUTO_LOAD_KEY: True}
 
+
 def create_instance(c_instance):
-    """ Creates and returns the Push script """
+    u""" Creates and returns the Push script """
 
-    UbermapDevicesPatches.apply_ubermap_patches()
-
+#FIX TO WORK WITH CLYPHX
+    from Ubermap import UbermapDevicesPatches
+#END OF FIX
+#FIX MULTIPLE VERSION BEING
+#    UbermapDevicesPatches.apply_ubermap_patches()
+    UbermapDevicesPatches.apply_ubermap_patches(True)
+#FIX END
     return Push(c_instance=c_instance)
--- Devices/Push2/__init__.py
+++ Devices/Push2/__init__.py
@@ -1,11 +1,12 @@
-# Embedded file name: /Users/versonator/Jenkins/live/output/mac_64_static/Release/python-bundle/MIDI Remote Scripts/Push2/__init__.py
-from __future__ import absolute_import, print_function
-
-from Ubermap import UbermapDevicesPatches
+# Embedded file name: c:\Jenkins\live\output\win_64_static\Release\python-bundle\MIDI Remote Scripts\Push2\__init__.py
+from __future__ import absolute_import, print_function, unicode_literals
+#FIX FOR USING WITH CLYPHX
+#from Ubermap import UbermapDevicesPatches
+#END OF FIX
 
 def get_capabilities():
     from ableton.v2.control_surface import capabilities as caps
-    return {caps.CONTROLLER_ID_KEY: caps.controller_id(vendor_id=10626, product_ids=[6503], model_name='Ableton Push 2'),
+    return {caps.CONTROLLER_ID_KEY: caps.controller_id(vendor_id=10626, product_ids=[6503], model_name=u'Ableton Push 2'),
      caps.PORTS_KEY: [caps.inport(props=[caps.HIDDEN, caps.NOTES_CC, caps.SCRIPT]),
                       caps.inport(props=[]),
                       caps.outport(props=[caps.HIDDEN,
@@ -13,7 +14,7 @@ def get_capabilities():
                        caps.SYNC,
                        caps.SCRIPT]),
                       caps.outport(props=[])],
-     caps.TYPE_KEY: 'push2',
+     caps.TYPE_KEY: u'push2',
      caps.AUTO_LOAD_KEY: True}
 
 
@@ -22,6 +23,11 @@ def create_instance(c_instance):
     from .push2_model import Root, Sender
     root = Root(sender=Sender(message_sink=c_instance.send_model_update, process_connected=c_instance.process_connected))
 
-    UbermapDevicesPatches.apply_ubermap_patches()
-
+#FIX FOR USING WITH CLYPHX
+    from Ubermap import UbermapDevicesPatches
+#END OF FIX
+#FIX MULTIPLE VERSION BEING
+    UbermapDevicesPatches.apply_ubermap_patches(False)
+#    UbermapDevicesPatches.apply_ubermap_patches()
+#FIX END
     return Push2(c_instance=c_instance, model=root)
